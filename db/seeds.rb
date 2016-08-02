2d43b3c69e8307a93c15ab301406c2f13c85e072
Jack Clackett <jclackett@gmail.com>
2016-08-02 18:14:55 +0200

    update to user model, and controller, updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-10.times do
-	User.create(
-		email: Faker::Internet.email,
-		name: Faker::Name.name,
-		access_token: Faker::Lorem.characters(30),
-		facebook_id: Faker::Number.number(10),
-	)
-end
+# 10.times do
+# 	User.create(
+# 		email: Faker::Internet.email,
+# 		name: Faker::Name.name,
+# 		access_token: Faker::Lorem.characters(30),
+# 		facebook_id: Faker::Number.number(10),
+# 	)
+# end
 
-10.times do 
+100.times do 
 	Event.create(
 		title: Faker::Hacker.noun,
 		description: Faker::Hacker.say_something_smart,
